5661b016948b7056576aa9be70e13074e1954d5b
jelenajelena11 <stojanovicj97@gmail.com>
2020-10-16 10:20:06 +0200

    Last commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HeaderComponent} from './header/header.component';
 import { MainComponent } from './main/main.component';
-import {FooterComponent} from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 
-  import { from } from 'rxjs';
+import { from } from 'rxjs';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { AdvisorsComponent } from './advisors/advisors.component';
 import { InvestorsComponent } from './investors/investors.component';
 
 const routes: Routes = [
-  {path: '', component:HeaderComponent},
   {path: '', component:MainComponent},
-  {path: '', component:FooterComponent},
   {path: '', component:AboutComponent},
   {path: '', component:PortfolioComponent},
   {path: '', component:AdvisorsComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
 import { AboutComponent } from './about/about.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
@@ -15,8 +12,6 @@ import { InvestorsComponent } from './investors/investors.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
     MainComponent,
     AboutComponent,
     PortfolioComponent,
